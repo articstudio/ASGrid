b7011b9209dabe93d433ed74e3608af137c9aa2f
Marc Mascort Bou <marc@marcmascort.com>
2016-08-09 19:05:49 +0200

    v2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,41 @@
+'use strict';
+
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
-    less = require('gulp-less'),
-    rename = require('gulp-rename');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+    config = {
+      sass: {
+        source: 'scss/ASGrid.scss',
+        target: 'dist/'
+      },
+      minify: {
+        source: ['dist/*.css', '!dist/*.min.css'],
+        target: 'dist/'
+      }
+    };
 
-gulp.task('compile', function () {
-  return gulp.src('./less/ASGrid.less', {read: true})
-      .pipe(less())
-      .pipe(gulp.dest('./build'))
+gulp.task('sass', function () {
+  return gulp.src(config.sass.source, {read: true})
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(config.sass.target));
+});
+
+gulp.task('minify', function () {
+  return gulp.src(config.minify.source)
+      .pipe(sourcemaps.init())
       .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(rename({suffix:'.min'}))
-      .pipe(gulp.dest('./build'));
+      .pipe(rename({suffix: '.min'}))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(config.minify.target));
+});
+
+gulp.task('make', function (cb) {
+  runSequence(
+    'sass',
+    'minify',
+    cb
+  );
 });
