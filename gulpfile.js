f81ae60123e5d42f8efe9bc266cb640350e5b79d
Marc Mascort Bou <marc@articstudio.com>
2017-10-05 16:16:14 +0200

    Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,43 @@
 'use strict';
 
 var gulp = require('gulp'),
-    cleanCSS = require('gulp-clean-css'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps'),
-    runSequence = require('run-sequence'),
-    config = {
-      sass: {
-        source: 'scss/ASGrid.scss',
-        target: 'dist/'
-      },
-      minify: {
-        source: ['dist/*.css', '!dist/*.min.css'],
-        target: 'dist/'
-      }
-    };
+        cleanCSS = require('gulp-clean-css'),
+        sass = require('gulp-sass'),
+        rename = require('gulp-rename'),
+        sourcemaps = require('gulp-sourcemaps'),
+        runSequence = require('run-sequence'),
+        path = require('path'),
+        config = {
+            cwd: './',
+            sass: {
+                source: 'scss/*.scss',
+                target: 'dist/'
+            },
+            minify: {
+                source: ['dist/*.css', '!dist/*.min.css'],
+                target: 'dist/'
+            }
+        };
 
 gulp.task('sass', function () {
-  return gulp.src(config.sass.source, {read: true})
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest(config.sass.target));
+    return gulp.src(path.join(config.cwd, config.sass.source), {read: true})
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest(path.join(config.cwd, config.sass.target)));
 });
 
 gulp.task('minify', function () {
-  return gulp.src(config.minify.source)
-      .pipe(sourcemaps.init())
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(config.minify.target));
+    return gulp.src(config.minify.source)
+            .pipe(sourcemaps.init())
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(path.join(config.cwd, config.minify.target)));
 });
 
 gulp.task('make', function (cb) {
-  runSequence(
-    'sass',
-    'minify',
-    cb
-  );
+    runSequence(
+            'sass',
+            'minify',
+            cb
+            );
 });
